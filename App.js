9f78512e1dd97a22e4f8dc446c4f2cb3f9f96b33
terrnit <terrnit.megaman@gmail.com>
2022-08-15 23:28:20 +0500

    add: react-navigation in project

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { View, Text, KeyboardAvoidingView, StyleSheet } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default function App() {
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.homeScreen}>
+      <Text style={styles.homeScreenText}>Home Screen Hooray!</Text>
     </View>
   );
 }
 
+function LoginScreen() {
+  return (
+    <KeyboardAvoidingView 
+      style={styles.loginScreen}
+      behavior="padding"
+      >
+      <Text style={styles.loginScreenText}>Login Screen Hooray!</Text>
+    </KeyboardAvoidingView>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
+
 const styles = StyleSheet.create({
-  container: {
+  loginScreen: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#333",
+    padding: 8,
+   
+  },
+  loginScreenText: {
+    color: "white",
+  },
+  homeScreen: {
+    flex: 1,
+    backgroundColor: "#333",
+    padding: 8,
+   
+  },
+  homeScreenText: {
+    color: "#334343",
   },
 });
